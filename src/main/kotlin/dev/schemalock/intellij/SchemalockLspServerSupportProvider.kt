ec126153e32ac1dddf16eb7f011555dcfe9f60c3
Lukasz Gogolin <lukasz.gogolin@gmail.com>
2026-06-07 23:53:33 +0200

    style: move LocalFileSystem to top-level import

--- src/main/kotlin/dev/schemalock/intellij/SchemalockLspServerSupportProvider.kt
+++ src/main/kotlin/dev/schemalock/intellij/SchemalockLspServerSupportProvider.kt
@@ -1,6 +1,7 @@
 package dev.schemalock.intellij
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
@@ -16,7 +17,7 @@ class SchemalockLspServerSupportProvider : LspServerSupportProvider {
     companion object {
         fun hasLockfile(project: Project): Boolean {
             val basePath = project.basePath ?: return false
-            val base = com.intellij.openapi.vfs.LocalFileSystem.getInstance()
+            val base = LocalFileSystem.getInstance()
                 .findFileByPath(basePath) ?: return false
             return base.findChild("schemalock.lock") != null
                 || base.findChild("schemalock.yaml") != null
